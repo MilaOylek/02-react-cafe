786bb10d0df9900355ad5002734aa796fbcc186b
MilaOylek <oylekmila@gmail.com>
2025-05-14 17:13:29 +0300

    update app tsx file

--- src/main.tsx
+++ src/main.tsx
@@ -4,7 +4,7 @@ import ReactDOM from "react-dom/client";
 import App from "./components/App/App";
 import "./index.css";
 
-ReactDOM.createRoot(document.getElementById("root")!).render(
+ReactDOM.createRoot(document.getElementById("root") as HTMLDivElement).render(
   <React.StrictMode>
     <App />
   </React.StrictMode>
