c6b07b3f8e32fc67304ed9acbb40b1d2ad341202
MilaOylek <oylekmila@gmail.com>
2025-05-14 15:24:43 +0300

    initial commit

---
+++ src/components/VoteOptions/VoteOptions.tsx
@@ -0,0 +1,32 @@
+import styles from "./VoteOptions.module.css";
+import type { VoteType } from "../../types/votes";
+
+interface Props {
+  onVote: (type: VoteType) => void;
+  onReset: () => void;
+  canReset: boolean;
+}
+
+export default function VoteOptions({ onVote, onReset, canReset }: Props) {
+  return (
+    <div className={styles.container}>
+      <button className={styles.button} onClick={() => onVote("good")}>
+        Good
+      </button>
+      <button className={styles.button} onClick={() => onVote("neutral")}>
+        Neutral
+      </button>
+      <button className={styles.button} onClick={() => onVote("bad")}>
+        Bad
+      </button>
+      {canReset && (
+        <button
+          className={`${styles.button} ${styles.reset}`}
+          onClick={onReset}
+        >
+          Reset
+        </button>
+      )}
+    </div>
+  );
+}
